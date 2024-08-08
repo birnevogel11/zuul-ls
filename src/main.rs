1b9444b7a35bc52624ba2f1c9f0f8010429be565
Birne Vogel <birnevogel11@gmail.com>
2024-08-08 19:39:00 +0200

    feat: understand the module policy a little bit

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,34 @@
-use clap::Parser;
+mod config;
+mod repo;
+mod study_parser;
+mod yaml_parse;
 
-mod parse;
+use crate::config::get_config;
+use crate::study_parser::temp;
+use std::path::Path;
 
-#[derive(Parser)]
-#[command(name = "zuul-search")]
-#[command(bin_name = "zuul-search")]
-enum ZuulSearchCli {
-    Roles(ZuulSearchCliRolesArgs),
-}
-
-#[derive(clap::Args)]
-#[command(version, about, long_about = "Search roles")]
-struct ZuulSearchCliRolesArgs {
-    #[arg(long)]
-    work_dir: Option<std::path::PathBuf>,
-}
+// use clap::Parser;
+//
+//
+// mod config;
+//
+// #[derive(Parser)]
+// #[command(name = "zuul-search")]
+// #[command(bin_name = "zuul-search")]
+// enum ZuulSearchCli {
+//     Roles(ZuulSearchCliRolesArgs),
+// }
+//
+// #[derive(clap::Args)]
+// #[command(version, about, long_about = "Search roles")]
+// struct ZuulSearchCliRolesArgs {
+//     #[arg(long)]
+//     work_dir: Option<std::path::PathBuf>,
+// }
 
 fn main() {
+    temp();
+    println!("{:?}", get_config(&Some(&Path::new("./config.yaml"))));
     // let args = ZuulSearchCli::parse();
     //
     // match args {
---
+++ src/study_parser.rs
@@ -0,0 +1,23 @@
+use std::path::{Path, PathBuf};
+
+use crate::config::{get_config, Config};
+use crate::repo::{list_repos, list_roles};
+
+pub fn temp() {
+    let repo_dirs = list_repos(PathBuf::from("/home/yen3/carit"));
+    let role_dirs = list_roles(&repo_dirs);
+    println!("{:?}", repo_dirs);
+    println!("{:?}", role_dirs);
+
+    // let docs = YValueLoader::load_from_str("test: [1, 2, 3]").unwrap();
+    // let doc = &docs[0]; // select the first YAML document
+    // println!("{:?}", doc);
+    //
+    // println!("{:?}", Config::read_config());
+    // println!("{:?}", Config::read_config_path(Path::new("./config.yaml")));
+    // println!(
+    //     "{:?}",
+    //     Config::validate_config(Config::read_config_path(Path::new("./config.yaml")).unwrap())
+    // );
+    println!("{:?}", get_config(&Some(&Path::new("./config.yaml"))));
+}
