dff9898fb83b5fb9fd0f6ce37961cfb21bc16966
Birne Vogel <birnevogel11@gmail.com>
2024-08-08 19:39:06 +0200

    feat: parse job

--- src/parser/job.rs
+++ src/parser/job.rs
@@ -1,4 +1,3 @@
-use std::path::Path;
 use std::path::PathBuf;
 use std::rc::Rc;
 
@@ -9,17 +8,91 @@ use crate::parser::common::parse_string_value;
 use crate::parser::common::StringLoc;
 use crate::parser::common::ZuulParse;
 use crate::parser::common::ZuulParseError;
-use crate::parser::yaml::{load_yvalue, YValue, YValueYaml};
+use crate::parser::yaml::{YValue, YValueYaml};
 use crate::search::path::retrieve_repo_path;
+
+use super::yaml::YValueYaml;
+
+#[derive(Clone, PartialEq, PartialOrd, Debug, Eq, Ord, Hash)]
+pub enum VarValue {
+    Null,
+    Integer(i64),
+    Boolean(bool),
+    Real(String),
+    String(String),
+    Array(Vec<VarValue>),
+    Hash(VarTable),
+}
+
+pub type VarTable = LinkedHashMap<StringLoc, VarValue>;
+
+fn parse_value(
+    value: &YValue,
+    path: &Rc<PathBuf>,
+    field_name: &str,
+) -> Result<VarValue, ZuulParseError> {
+    Ok(match value.value() {
+        YValueYaml::Real(v) => VarValue::Real(v.clone()),
+        YValueYaml::Integer(v) => VarValue::Integer(v.clone()),
+        YValueYaml::String(v) => VarValue::String(v.clone()),
+        YValueYaml::Boolean(v) => VarValue::Boolean(v.clone()),
+        YValueYaml::Array(vs) => {
+            let mut xs = Vec::new();
+            for v in vs {
+                xs.push(parse_value(v, path, field_name)?);
+            }
+            VarValue::Array(xs)
+        }
+        YValueYaml::Hash(vs) => {
+            let mut xs = VarTable::new();
+            for (key, value) in vs {
+                let key = parse_string_value(key, path, field_name)?;
+                let value = parse_value(value, path, key.as_str())?;
+                xs.insert(key, value);
+            }
+            VarValue::Hash(xs)
+        }
+        YValueYaml::Null => VarValue::Null,
+        YValueYaml::Alias(_) => unreachable!(),
+        YValueYaml::BadValue => unreachable!(),
+    })
+}
+
+fn parse_variables(
+    values: &YValue,
+    path: &Rc<PathBuf>,
+    field_name: &str,
+) -> Result<VarTable, ZuulParseError> {
+    if let Some(values) = values.as_hash() {
+        let mut vs = VarTable::new();
+        for (key, value) in values {
+            let key = parse_string_value(key, path, field_name)?;
+            let value = parse_value(value, path, key.as_str())?;
+            vs.insert(key, value);
+        }
+
+        Ok(vs)
+    } else {
+        Err(ZuulParseError::from(
+            format!("Failed to parse the value of {}", field_name)
+                .to_string()
+                .as_str(),
+            values,
+            path,
+        ))
+    }
+}
 
 #[derive(Clone, PartialEq, PartialOrd, Debug, Eq, Ord, Hash, Default)]
 pub struct Job {
     job_name: StringLoc,
+    description: Option<StringLoc>,
     parent: Option<StringLoc>,
     pre_run_playbooks: Vec<(StringLoc, PathBuf)>,
     run_playbooks: Vec<(StringLoc, PathBuf)>,
     post_run_playbooks: Vec<(StringLoc, PathBuf)>,
     clean_run_playbooks: Vec<(StringLoc, PathBuf)>,
+    variables: VarTable,
 }
 
 impl Job {
@@ -48,39 +121,42 @@ impl ZuulParse<Job> for Job {
         path: &Rc<PathBuf>,
     ) -> Result<Job, ZuulParseError> {
         let mut job_name = StringLoc::default();
+        let mut description: Option<StringLoc> = None;
         let mut parent: Option<StringLoc> = None;
         let mut pre_run_playbooks: Vec<(StringLoc, PathBuf)> = Vec::new();
         let mut run_playbooks: Vec<(StringLoc, PathBuf)> = Vec::new();
         let mut post_run_playbooks: Vec<(StringLoc, PathBuf)> = Vec::new();
         let mut clean_run_playbooks: Vec<(StringLoc, PathBuf)> = Vec::new();
+        let mut variables: VarTable = VarTable::new();
 
         for (key, value) in xs {
             match key.as_str() {
                 Some(key) => match key {
                     "job" => {
-                        let value = parse_string_value(value, path, "job")?;
-                        job_name = value;
+                        job_name = parse_string_value(value, path, "job")?;
                     }
                     "parent" => {
-                        let value = parse_string_value(value, path, "parent")?;
-                        parent = Some(value);
+                        parent = Some(parse_string_value(value, path, "parent")?);
                     }
-                    "prerun" => {
-                        let value = Job::parse_playbooks(value, path, "prerun")?;
-                        pre_run_playbooks = value;
+                    "description" => {
+                        description = Some(parse_string_value(value, path, "description")?);
+                    }
+                    "pre-run" => {
+                        pre_run_playbooks = Job::parse_playbooks(value, path, "pre-run")?;
                     }
                     "run" => {
-                        let value = Job::parse_playbooks(value, path, "run")?;
-                        run_playbooks = value;
+                        run_playbooks = Job::parse_playbooks(value, path, "run")?;
                     }
-                    "postrun" => {
-                        let value = Job::parse_playbooks(value, path, "postrun")?;
-                        post_run_playbooks = value;
+                    "post-run" => {
+                        post_run_playbooks = Job::parse_playbooks(value, path, "post-run")?;
                     }
-                    "cleanrun" => {
-                        let value = Job::parse_playbooks(value, path, "cleanrun")?;
-                        clean_run_playbooks = value;
+                    "clean-run" => {
+                        clean_run_playbooks = Job::parse_playbooks(value, path, "clean-run")?;
                     }
+                    "vars" => {
+                        variables = parse_variables(value, path, "vars")?;
+                    }
+                    "roles" => todo!(),
                     _ => {}
                 },
                 None => {
@@ -91,11 +167,13 @@ impl ZuulParse<Job> for Job {
 
         Ok(Job {
             job_name,
+            description,
             parent,
             pre_run_playbooks,
             run_playbooks,
             post_run_playbooks,
             clean_run_playbooks,
+            variables,
         })
     }
 }
