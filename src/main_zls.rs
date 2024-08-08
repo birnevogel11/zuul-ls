c791adb6fea71241678433d19d8ddbf5536d41e8
Birne Vogel <birnevogel11@gmail.com>
2024-08-08 19:41:38 +0200

    feat: add missing packages

--- src/main_zls.rs
+++ src/main_zls.rs
@@ -3,38 +3,46 @@ use std::env;
 use std::fs::File;
 use std::io::Write;
 
+use dashmap::DashMap;
+use tower_lsp::Client;
 use zuul_parser::search::path::to_path;
 
-fn init_logging(builder: &mut env_logger::Builder, raw_path: &str) {
-    let path = to_path(raw_path);
-    let target = Box::new(File::create(path).expect("Can't create file"));
-    builder
-        .target(env_logger::Target::Pipe(target))
-        .filter(None, log::LevelFilter::Debug)
-        .format(|buf, record| {
-            writeln!(
-                buf,
-                "[{} {} {}:{}] {}",
-                Local::now().format("%Y-%m-%d %H:%M:%S%.3f"),
-                record.level(),
-                record.file().unwrap_or("unknown"),
-                record.line().unwrap_or(0),
-                record.args()
-            )
-        })
-        .init();
+#[derive(Debug)]
+struct Backend {
+    client: Client,
 }
 
-fn main() {
+fn init_logging() -> Option<env_logger::Builder> {
     let mut builder = env_logger::Builder::new();
 
     if let Ok(path) = env::var("ZUUL_LS_LOG_PATH") {
-        init_logging(&mut builder, &path);
+        let path = to_path(&path);
+        let target = Box::new(File::create(path).expect("Can't create file"));
+        builder
+            .target(env_logger::Target::Pipe(target))
+            .filter(None, log::LevelFilter::Debug)
+            .format(|buf, record| {
+                writeln!(
+                    buf,
+                    "[{} {} {}:{}] {}",
+                    Local::now().format("%Y-%m-%d %H:%M:%S%.3f"),
+                    record.level(),
+                    record.file().unwrap_or("unknown"),
+                    record.line().unwrap_or(0),
+                    record.args()
+                )
+            })
+            .init();
+        Some(builder)
     } else {
         env_logger::init();
+        None
     }
+}
 
-    log::error!("error");
-    log::info!("info");
-    log::info!("debug");
+fn main() {
+    let _ = init_logging();
+
+    let stdin = tokio::io::stdin();
+    let stdout = tokio::io::stdout();
 }
