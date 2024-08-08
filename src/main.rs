d41239d58ee484a49a063921873338ff216bc071
Birne Vogel <birnevogel11@gmail.com>
2024-08-08 19:38:49 +0200

    feat: continue

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
-mod repo;
-
 use crate::repo::list_repos;
 use crate::repo::list_roles;
 
 use std::path::PathBuf;
 
+mod repo;
+mod yaml_parse;
+
 fn main() {
-    let repo_dirs = list_repos(PathBuf::from("/home/yen3"));
+    let repo_dirs = list_repos(PathBuf::from("/home/yen3/carit"));
     let role_dirs = list_roles(&repo_dirs);
     println!("{:?}", repo_dirs);
     println!("{:?}", role_dirs);
+
+    crate::yaml_parse::hello();
 }
